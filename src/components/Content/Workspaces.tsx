7934ad167dd0662f32f8adba7cd103cc36962247
adrianWadrzyk <adrian.wadrzyk.contact@gmail.com>
2021-06-11 17:09:32 +0200

    styled workspaces

--- src/components/Content/Workspaces.tsx
+++ src/components/Content/Workspaces.tsx
@@ -1,62 +1,81 @@
 import styled from 'styled-components';
 // Import Swiper React components
 import { Swiper, SwiperSlide } from 'swiper/react';
+import { useHistory } from 'react-router-dom';
 
 import 'swiper/swiper.scss';
 
 
 const Wrapper = styled.div`
-    background-color: red;
     display: flex;
     flex-wrap: wrap;
 `;
 
 const Header = styled.h2`
     width: 100%;
+    height: 30px;
+    line-height: 30px;
+    margin: 10px 0px 0;
+    font-size: 1.2em;
 `;
 
 const SingleContainer = styled.div`
     height:250px;
-    background-color: yellow;
     display: flex;
     flex-direction: column;
     justify-content: space-around;
-    margin: 0 10px;
+    margin: 10px 0px;
+    padding: 5px;
+    background-color: white;
+
+    &:hover { 
+        cursor: pointer;
+    }
 `;
 
 const Photo = styled.div`
-    height: 40%;
-    background-color: black;
+    height: 65%;
+    background: url('./media/contract.png');
+    background-position: center;
+    background-size: cover;
 `;
 
 const ContractName = styled.h3``;
 
-const ContractIcon = styled.img`
-    width: 20%;
+const ContractNumberOfUsers = styled.p`
+    padding-left: 10px;
 `;
 
 const InsideWrapper = styled.div`
-    margin: 0 auto;
-    width: 50%;
     display: flex;
-    justify-content: space-between;
+    width: 70%;
+    margin: 10px auto;
+    justify-content: space-around;
+    color: black;
     align-items: center;
+`;
+
+const ContractIcon = styled.img`
+    height: 40px;
 `;
 
 const Fotter = styled.div`
     display: flex;
     flex-wrap: wrap;
-    justify-content: space-around;
+    margin: 0 10px;
 `;
 
 const LatestUpdate = styled.p`
     width: 100%;
     text-align:left;
-    padding-top:15px;
-    padding-left: 20px;
-`
+    padding-top:5px;
+`;
 
 const Workspaces = () => { 
+    let history  = useHistory();
+    const redirect = () => { 
+        history.push('/corporation')
+    }
     return (
         <Wrapper>
             <Header>Workspaces</Header>
@@ -66,85 +85,85 @@ const Workspaces = () => {
              slidesPerView={3}
             >
                   <SwiperSlide>
-                  <SingleContainer>
-                    <Photo> Zdjęcie</Photo>
+                  <SingleContainer onClick={redirect}>
+                    <Photo></Photo>
                     <InsideWrapper>
                         <ContractIcon src="./media/administration.png"></ContractIcon>
                         <ContractName>Client contract</ContractName>
                     </InsideWrapper>
                     <Fotter>
-                        <p>Contract</p>
-                        <p>150 users</p>
+                        <ContractName>Contract</ContractName>
+                        <ContractNumberOfUsers>150 users</ContractNumberOfUsers>
                         <LatestUpdate>Latest update 2 day ago</LatestUpdate>
                     </Fotter>
-                 </SingleContainer>
+                 </SingleContainer >
                   </SwiperSlide>
                   <SwiperSlide>
-                  <SingleContainer>
-                    <Photo> Zdjęcie</Photo>
+                  <SingleContainer onClick={redirect}>
+                    <Photo></Photo>
                     <InsideWrapper>
                         <ContractIcon src="./media/administration.png"></ContractIcon>
                         <ContractName>Client contract</ContractName>
                     </InsideWrapper>
                     <Fotter>
-                        <p>Contract</p>
-                        <p>150 users</p>
+                        <ContractName>Contract</ContractName>
+                        <ContractNumberOfUsers>150 users</ContractNumberOfUsers>
                         <LatestUpdate>Latest update 2 day ago</LatestUpdate>
                     </Fotter>
                  </SingleContainer>
                   </SwiperSlide>
                 <SwiperSlide>
-                  <SingleContainer>
-                    <Photo> Zdjęcie</Photo>
+                  <SingleContainer onClick={redirect}>
+                    <Photo></Photo>
                     <InsideWrapper>
                         <ContractIcon src="./media/administration.png"></ContractIcon>
                         <ContractName>Client contract</ContractName>
                     </InsideWrapper>
                     <Fotter>
-                        <p>Contract</p>
-                        <p>150 users</p>
+                        <ContractName>Contract</ContractName>
+                        <ContractNumberOfUsers>150 users</ContractNumberOfUsers>
                         <LatestUpdate>Latest update 2 day ago</LatestUpdate>
                     </Fotter>
                  </SingleContainer>
                   </SwiperSlide>
                   <SwiperSlide>
-                  <SingleContainer>
-                    <Photo> Zdjęcie</Photo>
+                  <SingleContainer onClick={redirect}>
+                    <Photo></Photo>
                     <InsideWrapper>
                         <ContractIcon src="./media/administration.png"></ContractIcon>
                         <ContractName>Client contract</ContractName>
                     </InsideWrapper>
                     <Fotter>
-                        <p>Contract</p>
-                        <p>150 users</p>
+                        <ContractName>Contract</ContractName>
+                        <ContractNumberOfUsers>150 users</ContractNumberOfUsers>
                         <LatestUpdate>Latest update 2 day ago</LatestUpdate>
                     </Fotter>
                  </SingleContainer>
                   </SwiperSlide>
                   <SwiperSlide>
-                  <SingleContainer>
-                    <Photo> Zdjęcie</Photo>
+                  <SingleContainer onClick={redirect}>
+                    <Photo></Photo>
                     <InsideWrapper>
                         <ContractIcon src="./media/administration.png"></ContractIcon>
                         <ContractName>Client contract</ContractName>
                     </InsideWrapper>
                     <Fotter>
-                        <p>Contract</p>
-                        <p>150 users</p>
+                        <ContractName>Contract</ContractName>
+                        <ContractNumberOfUsers>150 users</ContractNumberOfUsers>
                         <LatestUpdate>Latest update 2 day ago</LatestUpdate>
                     </Fotter>
                  </SingleContainer>
                   </SwiperSlide>
                   <SwiperSlide>
-                  <SingleContainer>
-                    <Photo> Zdjęcie</Photo>
+                  <SingleContainer onClick={redirect}>
+                    <Photo></Photo>
                     <InsideWrapper>
                         <ContractIcon src="./media/administration.png"></ContractIcon>
                         <ContractName>Client contract</ContractName>
                     </InsideWrapper>
                     <Fotter>
-                        <p>Contract</p>
-                        <p>150 users</p>
+                        <ContractName>Contract</ContractName>
+                        <ContractNumberOfUsers>150 users</ContractNumberOfUsers>
                         <LatestUpdate>Latest update 2 day ago</LatestUpdate>
                     </Fotter>
                  </SingleContainer>
